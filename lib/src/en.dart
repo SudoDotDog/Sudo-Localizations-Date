727c713857eff3d3a8c4e677a659d890a46a49e5
WMXPY <wm@mengw.io>
2021-03-20 15:11:19 -0500

    fix declare matching

--- lib/src/en.dart
+++ lib/src/en.dart
@@ -1,5 +1,11 @@
+import 'package:localizations_date/src/declare/date.dart';
 import 'package:localizations_date/src/declare/declare.dart';
+import 'package:localizations_date/src/declare/time.dart';
 
-LocalizationDateFormatFunction localizationDateEnglishFormat = (DateTime date) {
+LocalizationDateFormatFunction localizationDateEnglishFormat = (
+  DateTime date, {
+  LocalizationDateFormatDateConfig dateConfig,
+  LocalizationDateFormatTimeConfig timeConfig,
+}) {
   return date.year.toString();
 };
--- lib/src/zh.dart
+++ lib/src/zh.dart
@@ -1,7 +1,11 @@
+import 'package:localizations_date/src/declare/date.dart';
 import 'package:localizations_date/src/declare/declare.dart';
+import 'package:localizations_date/src/declare/time.dart';
 
 LocalizationDateFormatFunction localizationDateChineseFormat = (
-  DateTime date,
-) {
+  DateTime date, {
+  LocalizationDateFormatDateConfig dateConfig,
+  LocalizationDateFormatTimeConfig timeConfig,
+}) {
   return date.year.toString();
 };
