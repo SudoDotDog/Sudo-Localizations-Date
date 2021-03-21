c143f1286724166df8dc640739dc18f6f1a2321b
WMXPY <wm@mengw.io>
2021-03-21 13:57:51 -0500

    fix import

--- test/unit/local.dart
+++ test/unit/local.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:localizations_date/src/declare/date.dart';
 import 'package:localizations_date/src/declare/time.dart';
