d6a9fc4b95b29e22534e4d593712b6210eeb3039
WMXPY <wm@mengw.io>
2021-03-21 13:42:17 -0500

    finish zh format

--- lib/src/zh.dart
+++ lib/src/zh.dart
@@ -7,5 +7,48 @@ LocalizationDateFormatFunction localizationDateChineseFormat = (
   LocalizationDateFormatDateConfig dateConfig,
   LocalizationDateFormatTimeConfig timeConfig,
 }) {
-  return date.year.toString();
+  final List<String> builder = [];
+
+  if (dateConfig.ensureIncludes()) {
+    if (dateConfig.includesYear) {
+      builder.add(date.year.toString());
+      if (dateConfig.includesMonth) {
+        builder.add("-");
+      }
+    }
+
+    if (dateConfig.includesMonth) {
+      builder.add(date.month.toString());
+      if (dateConfig.includesDay) {
+        builder.add("-");
+      }
+    }
+
+    if (dateConfig.includesDay) {
+      builder.add(date.day.toString());
+    }
+    builder.add(' ');
+  }
+
+  if (timeConfig.ensureIncludes()) {
+    if (timeConfig.includesHour) {
+      builder.add(date.hour.toString());
+      if (timeConfig.includesMinute) {
+        builder.add(":");
+      }
+    }
+
+    if (timeConfig.includesMinute) {
+      builder.add(date.minute.toString());
+      if (timeConfig.includesSecond) {
+        builder.add(":");
+      }
+    }
+
+    if (timeConfig.includesSecond) {
+      builder.add(date.second.toString());
+    }
+  }
+
+  return builder.join();
 };
