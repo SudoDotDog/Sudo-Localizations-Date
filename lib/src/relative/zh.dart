28c30ac7c77b2de9fb0374b98313bb66e464c50c
WMXPY <wm@mengw.io>
2021-03-22 14:18:15 -0500

    add chinese relateive map

---
+++ lib/src/relative/zh.dart
@@ -0,0 +1,31 @@
+const Map<String, String> localizationDateChineseRelativeMap = {
+  // Space
+  'space': '',
+
+  // Now
+  'about-now': '马上',
+  'just-now': '刚刚',
+
+  // Less than a minute
+  'less-than-a-minute': '不到一分钟',
+
+  // Minutes ago
+  'minutes-ago': '分钟前',
+  'minutes-later': '分钟后',
+
+  // Hours ago
+  'hours-ago': '小时前',
+  'hours-later': '小时后',
+
+  // Days ago
+  'days-ago': '天前',
+  'days-later': '天后',
+
+  // Months ago
+  'months-ago': '个月前',
+  'months-later': '个月后',
+
+  // Years ago
+  'years-ago': '年前',
+  'years-later': '年后',
+};
