c8d79be3f085073012a56c4e74ba0a283ed4535e
WMXPY <wm@mengw.io>
2021-03-22 13:25:47 -0500

    parse en

--- lib/src/relative/en.dart
+++ lib/src/relative/en.dart
@@ -2,9 +2,20 @@ import 'package:localizations_date/src/relative/declare/declare.dart';
 
 RelativeFormatFunction localizationDateEnglishRelativeFormat = (
   DateTime date, {
+  DateTime now,
   RelativeFormatConfig config,
 }) {
   final List<String> builder = [];
+
+  if (now == null) {
+    now = DateTime.now();
+  }
+
+  final int verticalDifference =
+      date.millisecondsSinceEpoch - now.millisecondsSinceEpoch;
+
+  final int abstractDifference = verticalDifference.abs();
+  bool isBefore = verticalDifference > 0;
 
   return builder.join();
 };
