f22620f660daf9a5c64fcac5f65b55e43769e53e
WMXPY <wm@mengw.io>
2021-03-23 12:55:17 -0500

    init texting

--- lib/src/util/texting.dart
+++ lib/src/util/texting.dart
@@ -1,5 +1,19 @@
-String getMonthTexting(int month) {
+String getMonthTexting(
+  int month, {
+  bool fullText,
+}) {
   if (month > 12 || month < 1) {
     return "Invalid Month";
   }
+
+  if (fullText == null) {
+    fullText = false;
+  }
+
+  switch (month) {
+    case 1:
+      return fullText ? "January" : "Jan";
+  }
+
+  return "Invalid Month";
 }
