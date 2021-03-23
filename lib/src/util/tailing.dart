46ae3694b3a3703556d662f571f5ec0972108d55
WMXPY <wm@mengw.io>
2021-03-23 12:48:05 -0500

    finish tailing

--- lib/src/util/tailing.dart
+++ lib/src/util/tailing.dart
@@ -1,3 +1,19 @@
 String addMonthNumberTailing(int month) {
-  return "";
+  if (month > 31 || month < 1) {
+    return "Invalid Month";
+  }
+
+  if (month == 1 || month == 21 || month == 31) {
+    return "${month}st";
+  }
+
+  if (month == 2 || month == 22) {
+    return "${month}nd";
+  }
+
+  if (month == 3 || month == 23) {
+    return "${month}rd";
+  }
+
+  return "${month}th";
 }
